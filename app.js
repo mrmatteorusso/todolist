f6971b9d65e3d738d47bc5382b8a89f308f6cd50
mrmatteorusso <80893528+mrmatteorusso@users.noreply.github.com>
2021-08-04 15:34:21 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
 
+const generateTemplate = todo => {
+    const html = `
+    <li>
+        <span>${todo}</span>
+        <i class="far fa-trash-alt delete"></i>
+    </li>`
+    list.innerHTML += html;
+    console.log('ciao')
+
+}
+
+addForm.addEventListener('submit', e => {
+
+    const todo = addForm.add.value.trim();
+    e.preventDefault();
+    if (todo.length) {
+        generateTemplate(todo);  
+        addForm.reset();  
+
+    }
+
+}); 
+
+list.addEventListener('click', e => {
+console.log('clicked')
+if (e.target.classList.contains('delete')) {
+    e.target.parentElement.remove();
+}
+
+})
