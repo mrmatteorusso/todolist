8e1b18bce3a13da66cc7cc6c45c0e5e2284b3b61
mrmatteorusso <mrmatteorusso@gmail.com>
2021-08-04 15:24:15 +0200

    index.html and app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+
+const generateTemplate = todo => {
+    const html = `
+    <li>
+        <span>${todo}</span>
+        <i class="far fa-trash-alt delete"></i>
+    </li>`
+    list.innerHTML += html;
+
+}
+
+addForm.addEventListener('submit', e => {
+
+    const todo = addForm.add.value.trim();
+    e.preventDefault();
+    if (todo.length) {
+        generateTemplate(todo);  
+        addForm.reset();  
+
+    }
+
+}); 
+
+list.addEventListener('click', e => {
+console.log('clicked')
+if (e.target.classList.contains('delete')) {
+    e.target.parentElement.remove();
+}
+
+})
