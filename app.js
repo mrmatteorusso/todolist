595628f616fcb36b5fc21f515b41bab0ff58f1f4
matteobarone <barmat90@gmail.com>
2021-08-30 18:15:12 +0200

    read from localStorage at the beginning

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const addForm = document.querySelector('.add');
 const list = document.querySelector('.todos');
+// salvato gli items dal local storage in una variabile
+let storedItems = localStorage.getItem('tasks');
 
 const generateTemplate = todo => {
     const html = `
@@ -7,48 +9,38 @@ const generateTemplate = todo => {
         <span>${todo}</span>
         <i class="far fa-trash-alt delete"></i>
     </li>`
+    
     list.innerHTML += html;
-    console.log('ciao')
-
+    console.log('created!');
 }
 
-
+if (!storedItems) {
+    storedItems = [];
+} else {
+    storedItems = JSON.parse(storedItems);
+    storedItems.forEach(item => {
+        generateTemplate(item);
+    });
+}
 
 addForm.addEventListener('submit', e => {
-
     const todo = addForm.add.value.trim();
     e.preventDefault();
     if (todo.length) {
-        let tasks;
-
-        // in verità questo controllo è corretto ma va fatto all'inizio, allo start time
-        // non qui, perchè qui avviene solamente quando fai submit, e noi vogliamo popolare i task subito
-        // una volta popolati i tasks, vanno riempiti (generateTemplate)
-        // In questo caso va fatta un altra operazione del localStorage: il set
-        // perchè vogliamo settare il nuovo item che abbiamo appena inserito dopo aver fatto submit
-        if(localStorage.getItem('tasks') === null) {
-            tasks = [];
-        } else {
-            tasks = JSON.parse(localStorage.getItem('tasks'));
-        }
-
         generateTemplate(todo); 
-        tasks.push(todo);
-        localStorage.setItem('tasks', JSON.stringify(tasks))
-        addForm.reset();  
-        // console.log(tasks.forEach( e => console.log(e)))
-
+        storedItems.push(todo);
+        localStorage.setItem('tasks', JSON.stringify(storedItems))
+        addForm.reset();
     }
-
-}); 
-
+});
 
 list.addEventListener('click', e => {
+    console.log(e.target);
     // so che è una piccolezza, ma occhio all'indentazione
     // ti da struttura e una logica di lettura migliore del codice
     console.log('clicked')
     if (e.target.classList.contains('delete')) {
         e.target.parentElement.remove();
+        console.dir(e.target.parentElement.firstElementChild.innerText);
     }
 })
-
